83d13eaa587eab453f37eb7f18905cf0d701571e
Hyunwooz <107661525+Hyunwooz@users.noreply.github.com>
2025-02-13 00:05:52 +0900

    test : modify mock data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,30 @@
-import StatusCardBox from '@/components/statusContainer';
+import StatusCardBox from '@/components/status/statusContainer';
+
+import { TaskStatus } from '@/shared/types/status';
 
 const Home = () => {
-  const statusMocData = [
-    { name: 'To Do', tasks: 4, ringColor: 'bg-blue-300' },
-    { name: 'In Progress', tasks: 2, ringColor: 'bg-yellow-300' },
-    { name: 'On Hold', tasks: 6, ringColor: 'bg-red-300' },
-    { name: 'Done', tasks: 8, ringColor: 'bg-green-300' },
+  const statusMocData: TaskStatus[] = [
+    { name: 'To Do', totalTasks: 4, color: 'blue', tasks: [] },
+    {
+      name: 'In Progress',
+      totalTasks: 2,
+      color: 'yellow',
+      tasks: [{ title: '할 일 제목', index: 0, toDoLists: ['첫 번째 할 일'] }],
+    },
+    { name: 'On Hold', totalTasks: 6, color: 'red', tasks: [] },
+    {
+      name: 'Done',
+      totalTasks: 8,
+      color: 'green',
+      tasks: [{ title: '할 일 제목', index: 0, toDoLists: ['첫 번째 할 일'] }],
+    },
   ];
+
   return (
     <div className='grid grid-cols-1 gap-14 md:grid-cols-2 xl:grid-cols-4'>
       {statusMocData &&
-        statusMocData.map((data, index) => {
-          return (
-            <StatusCardBox
-              key={index}
-              name={data.name}
-              tasks={data.tasks}
-              ringColor={data.ringColor}
-            />
-          );
+        statusMocData.map((status, index) => {
+          return <StatusCardBox key={index} status={status} />;
         })}
     </div>
   );
