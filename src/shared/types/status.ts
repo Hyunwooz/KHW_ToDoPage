8c4531cdb34869ea9e7e4cfc7069f68052b82d99
Hyunwooz <107661525+Hyunwooz@users.noreply.github.com>
2025-02-13 00:02:59 +0900

    refactor : rename StatusColor and Task types

--- src/shared/types/status.ts
+++ src/shared/types/status.ts
@@ -1,8 +1,9 @@
 export type StatusColor = 'green' | 'blue' | 'red' | 'yellow';
+export type Task = { title: string; index: number; toDoLists: string[] };
 
 export interface TaskStatus {
   name: string;
   totalTasks: number;
   color: StatusColor;
-  toDoLists: string[];
+  tasks: Task[];
 }
