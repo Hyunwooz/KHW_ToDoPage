f929075b3cbeea0b0c388f4c12c5f9f0abe84778
Hyunwooz <107661525+Hyunwooz@users.noreply.github.com>
2025-02-13 21:56:33 +0900

    refactor : Header 위치 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,25 +22,6 @@ export default function RootLayout({
   return (
     <html lang='ko'>
       <body className={`${pretendard.className}`}>
-        <header className='px-4 py-14'>
-          <div className='mx-auto flex max-w-primary flex-col justify-between gap-4 lg:flex-row lg:items-end'>
-            <div>
-              <h1 className='mb-4 text-5xl font-extrabold'>To Do List</h1>
-              <div>
-                <p className='font-medium text-gray-700'>
-                  Here all tasks in the project.
-                </p>
-                <p className='font-medium text-gray-700'>
-                  You will find information for each as well as assignees
-                  responsible for completion
-                </p>
-              </div>
-            </div>
-            <button className='h-12 w-32 rounded-xl bg-black font-bold text-white'>
-              + New Board
-            </button>
-          </div>
-        </header>
         <main className='mx-auto min-h-[70vh] max-w-primary px-4'>
           {children}
         </main>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,29 @@
+'use client';
+
 import BoardContainer from '@/components/boardContainer';
 
 const Home = () => {
   return (
     <div>
+      <header className='px-4 py-14'>
+        <div className='mx-auto flex max-w-primary flex-col justify-between gap-4 lg:flex-row lg:items-end'>
+          <div>
+            <h1 className='mb-4 text-5xl font-extrabold'>To Do List</h1>
+            <div>
+              <p className='font-medium text-gray-700'>
+                Here all tasks in the project.
+              </p>
+              <p className='font-medium text-gray-700'>
+                You will find information for each as well as assignees
+                responsible for completion
+              </p>
+            </div>
+          </div>
+          <button className='h-12 w-32 rounded-xl bg-black font-bold text-white'>
+            + New Board
+          </button>
+        </div>
+      </header>
       <BoardContainer />
     </div>
   );
