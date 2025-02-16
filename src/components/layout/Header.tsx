8c666b444aded15d4bce809dac2423aff3482c32
Hyunwooz <107661525+Hyunwooz@users.noreply.github.com>
2025-02-16 09:55:35 +0900

    refactor : navItem 분리 및 문구 변경

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,55 +1,20 @@
 'use client';
 
-import { useState } from 'react';
 import SearchBar from './search/SearchBar';
 import NavLink from '@/components/layout/navigation/NavLink';
 import Link from 'next/link';
+import { navItems } from './navigation/navItems';
+
 const Header = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-
-  const navItems = [
-    { href: '/', label: '메인' },
-    { href: '/stats', label: '통계' },
-    { href: '/archive', label: '보관함' },
-  ];
-
   return (
     <header className='border-b border-solid border-gray-200 px-4 py-4'>
-      <div className='mx-auto flex max-w-primary flex-col justify-between gap-4 lg:flex-row lg:items-center'>
-        <div className='flex items-center justify-between lg:gap-10'>
+      <div className='mx-auto flex max-w-primary flex-col justify-between gap-4 md:flex-row md:items-center'>
+        <div className='flex flex-col items-start justify-between gap-2 md:flex-row md:items-center md:gap-10'>
           <Link href='/'>
-            <h1 className='text-4xl font-extrabold text-blue-700'>TaskFlow</h1>
+            <h1 className='text-4xl font-extrabold text-blue-700'>ToDo</h1>
           </Link>
 
-          <button
-            onClick={() => setIsMenuOpen(!isMenuOpen)}
-            className='lg:hidden'
-          >
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              className='h-6 w-6'
-              fill='none'
-              viewBox='0 0 24 24'
-              stroke='currentColor'
-            >
-              <path
-                strokeLinecap='round'
-                strokeLinejoin='round'
-                strokeWidth={2}
-                d='M4 6h16M4 12h16M4 18h16'
-              />
-            </svg>
-          </button>
-
-          <nav className='hidden items-center gap-4 lg:flex'>
-            {navItems.map((item) => (
-              <NavLink key={item.href} {...item} />
-            ))}
-          </nav>
-        </div>
-
-        <div className={`${isMenuOpen ? 'block' : 'hidden'} lg:hidden`}>
-          <nav className='flex flex-col gap-2'>
+          <nav className='flex items-center gap-4'>
             {navItems.map((item) => (
               <NavLink key={item.href} {...item} />
             ))}
---
+++ src/components/layout/navigation/navItems.ts
@@ -0,0 +1,5 @@
+export const navItems = [
+  { href: '/', label: '메인' },
+  { href: '/stats', label: '통계' },
+  { href: '/archive', label: '보관함' },
+];
