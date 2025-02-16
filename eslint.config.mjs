1558ae8d980e39d22365b916316cb079ef4c6661
Hyunwooz <107661525+Hyunwooz@users.noreply.github.com>
2025-02-16 19:14:21 +0900

    chore : 불필요 eslint 삭제

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,14 +9,10 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-// seed.ts는 실행 목적의 파일 (ESLint 검사 불필요)
-// CJS 와 ES Module 충돌 방지
-// prisma 공식 문서 참조 : https://www.prisma.io/docs/orm/prisma-migrate/workflows/seeding
-
 const eslintConfig = [
   ...compat.extends('next/core-web-vitals', 'next/typescript'),
   {
-    ignores: ['prisma/seed.ts'], // `prisma/seed.ts`를 ESLint 검사에서 제외
+    ignores: [''],
   },
 ];
 
