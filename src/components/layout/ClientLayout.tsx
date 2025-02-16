606f9c1eb51fad89d60d4e6ad8a6feb0ee0fd2fc
Hyunwooz <107661525+Hyunwooz@users.noreply.github.com>
2025-02-16 09:40:25 +0900

    feat : ClientLayout에 Header 적용 및 Hydration 에러 방지 추가

---
+++ src/components/layout/ClientLayout.tsx
@@ -0,0 +1,33 @@
+'use client';
+
+import Header from '@/components/layout/Header';
+import { useState, useEffect } from 'react';
+
+interface ClientLayoutProps {
+  children: React.ReactNode;
+}
+
+export default function ClientLayout({ children }: ClientLayoutProps) {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return (
+      <div className='flex min-h-screen items-center justify-center'>
+        <div className='h-16 w-16 animate-spin rounded-full border-b-2 border-t-2 border-blue-700'></div>
+      </div>
+    );
+  }
+
+  return (
+    <>
+      <Header />
+      <main className='mx-auto min-h-[70vh] max-w-primary px-4 py-4 2xl:px-0'>
+        {children}
+      </main>
+    </>
+  );
+}
